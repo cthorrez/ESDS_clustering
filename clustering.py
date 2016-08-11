069f71e31bbd58b1d3284a4d3cef94164a8ed3ef
Clayton Thorrez <thorrez@umich.edu>
2016-08-10 20:21:28 -0400

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -1,3 +1,5 @@
+# Code to cluster professional LoL playres into their roles based on kills, deaths, assists, cs per gae averages
+# Clayton Thorrez 2016
 from sklearn.cluster import KMeans, SpectralClustering
 
 class Player:
@@ -12,7 +14,7 @@ if __name__ == "__main__":
     players = {}
     data = []
     names = []
-    data_file = open("kda_50.txt", "r")
+    data_file = open("kda_200.txt", "r")
     for line in data_file:
         fields = line.split(",")
         data.append([float(fields[1]), float(fields[3]), float(fields[4]), float(fields[4])])
@@ -27,8 +29,6 @@ if __name__ == "__main__":
     for x in range(len(data)):
         pred = labels[x]
         name = names[x]
-        if " " in name:
-            name = name[0:name.find("(")]
         boxes[pred].append(name.ljust(10))
 
     print len(boxes[0]), len(boxes[1]), len(boxes[2]), len(boxes[3]), len(boxes[4])
@@ -37,8 +37,6 @@ if __name__ == "__main__":
     for box in boxes:
         while len(box) < biggest:
             box.append("            ")
-            
-
 
     for x in range(biggest):
         print boxes[0][x], "\t",boxes[1][x],"\t", boxes[2][x],"\t", boxes[3][x],"\t", boxes[4][x]
